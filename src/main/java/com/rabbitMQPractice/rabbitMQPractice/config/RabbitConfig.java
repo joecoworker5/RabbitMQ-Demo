e22c0baeb59190231b43c02baeafe85627044226
joe.su <joe.su@tenmax.io>
2025-01-19 11:12:48 +0800

    PostMessageProcessor

--- src/main/java/com/rabbitMQPractice/rabbitMQPractice/config/RabbitConfig.java
+++ src/main/java/com/rabbitMQPractice/rabbitMQPractice/config/RabbitConfig.java
@@ -16,4 +16,6 @@ public class RabbitConfig {
         }});
         return converter;
     }
+
+
 }
--- src/main/java/com/rabbitMQPractice/rabbitMQPractice/direct/DirectProducer.java
+++ src/main/java/com/rabbitMQPractice/rabbitMQPractice/direct/DirectProducer.java
@@ -1,5 +1,6 @@
 package com.rabbitMQPractice.rabbitMQPractice.direct;
 
+import org.springframework.amqp.core.MessageDeliveryMode;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -13,6 +14,9 @@ public class DirectProducer {
     private RabbitTemplate rabbitTemplate;
 
     public void send(Student student){
-        rabbitTemplate.convertAndSend("direct_queue", student);
+        rabbitTemplate.convertAndSend("direct_queue", student,  message -> {
+            message.getMessageProperties().setDeliveryMode(MessageDeliveryMode.PERSISTENT); //消息持久化, 確保訊息不會丟失, 預設也是
+            return message;
+        });
     }
 }
